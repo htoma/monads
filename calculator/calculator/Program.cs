4b6c4e61a759d5344a5ce2a9e9013b28eb657383
htoma <horia.toma@yahoo.com>
2011-07-20 00:43:34 +0200

    first version

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace calculator
+{
+    // inspired by bartosz milewski, see: http://bartoszmilewski.wordpress.com/2011/03/17/monads-for-the-curious-programmer-part-3/
+
+    // compromised on Pop and Peek that use default(T) instead of throwing
+    // todo (htoma): combine with enriched type that swallows exceptions
+
+    // can be templated only on T, but used second generic parameter to emphasize correct processing inside Bind
+    class Tuple<T, U>
+    {
+        public U Value;
+        public IState<T> Calculator;
+
+        public Tuple(U value, IState<T> calculator)
+        {
+            Value = value;
+            Calculator = calculator;
+        }
+    }
+
+    static class Extensions
+    {
+        public static Func<IState<T>, Tuple<T, T>> Pop<T>()
+        {
+            return x => new Tuple<T, T>(x.Peek(), x.Pop());
+        }
+
+        // basic type constructor using Push
+        public static Func<IState<T>, Tuple<T, T>> Push<T>(T toBePushed)
+        {
+            return x => new Tuple<T, T>(x.Peek(), x.Push(toBePushed));
+        }
+
+        public static Func<IState<T>, Tuple<T, T>> AddTwo<T>(Func<T, T, T> add)
+        {
+            return x => new Tuple<T, T>(default(T),
+                x.Push(add(x.Peek(), x.Pop().Peek())));
+        }
+
+        // binder
+        public static Func<IState<T>, Tuple<T, B>> Bind<T, A, B>(this Func<IState<T>, Tuple<T, A>> act,
+            Func<A, Func<IState<T>, Tuple<T, B>>> func)
+        {
+            return x =>
+                       {
+                           var tmp = act(x);
+                           var tmpFunc = func(tmp.Value);
+                           return tmpFunc(tmp.Calculator);
+                       };
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // empty list
+            IState<int> s1 = State<int>.Empty;
+
+            // list having two elements
+            IState<int> s2 = s1.Push(10).Push(2);
+
+            // add an element, add stack(top) + stack(top - 1) and extract top
+            var funcFirst = Extensions.Push(1).
+                Bind(x => Extensions.AddTwo<int>((p, q) => p + q)).Bind(x => Extensions.Pop<int>());
+            Console.WriteLine(funcFirst(s1).Value);
+            Console.WriteLine(funcFirst(s2).Value);
+
+            // add two elements, add stack(top) + stack(top - 1) and extract top 
+            var funcSecond = Extensions.Push(1).Bind(x => Extensions.Push(4))
+                .Bind(x => Extensions.AddTwo<int>((p, q) => p + q)).Bind(x => Extensions.Pop<int>());
+
+            Console.WriteLine(funcSecond(s2).Value);
+        }
+    }
+}
---
+++ calculator/calculator/State.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace calculator
+{
+    // credits to eric lippert, see: http://blogs.msdn.com/b/ericlippert/archive/2007/12/04/immutability-in-c-part-two-a-simple-immutable-stack.aspx
+
+    public interface IState<T> : IEnumerable<T>
+    {
+        IState<T> Push(T value);
+        IState<T> Pop();
+        T Peek();
+        bool IsEmpty { get; }
+    }
+    
+    // renamed ImmutableStack to State for ease of notation use
+    public class State<T> : IState<T>
+    {
+        private sealed class EmptyStack : IState<T>
+        {
+            public bool IsEmpty
+            {
+                get { return true; }
+            }
+
+            public T Peek()
+            {
+                return default(T);
+            } //throw new Exception("Empty stack"); 
+            
+            public IState<T> Push(T value)
+            {
+                return new State<T>(value, this);
+            }
+
+            public IState<T> Pop()
+            {
+                return this;
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                yield break;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+        private static readonly EmptyStack empty = new EmptyStack();
+        public static IState<T> Empty { get { return empty; } }
+        private readonly T head;
+        private readonly IState<T> tail;
+        
+        private State(T head, IState<T> tail)
+        {
+            this.head = head;
+            this.tail = tail;
+        }
+        public bool IsEmpty { get { return false; } }
+        public T Peek() { return head; }
+        public IState<T> Pop() { return tail; }
+        public IState<T> Push(T value) { return new State<T>(value, this); }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(IState<T> state = this; !state.IsEmpty ; state = state.Pop())
+                yield return state.Peek();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {return GetEnumerator();}
+    }
+}
